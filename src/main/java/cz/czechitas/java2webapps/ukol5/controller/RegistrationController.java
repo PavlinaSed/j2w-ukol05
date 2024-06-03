ce890972be43e2f26f5c78a117dce41416277fef
Pavlina Sedlackova <p1sedlackova@gmail.com>
2024-06-03 23:28:22 +0200

    feat:age validation in controller (Part 8 completed)need to fix #if directive in registered.ftlh
    need to fix erasing of radio button selection

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistrationController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistrationController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
  */
@@ -25,13 +28,36 @@ public class RegistrationController {
 
     @PostMapping("")
     public Object form(@Valid @ModelAttribute("form") RegistrationForm form, BindingResult bindingResult) {
+        //TODO when wrong value of birthdate is submitted, the previously chosen radio button gender selection is erased
 
+        if (!bindingResult.hasErrors()) {
+            Period period = Period.between(form.getBirthDate(), LocalDate.now());
+            int vek = period.getYears();
+            if (vek < 9 || vek > 15) {
+                bindingResult.rejectValue("birthDate","","NOT VALID AGE: Sorry, but only children in the age of 9-15years can be registered!");
+                return "/form";
+            }
+            return new ModelAndView("/registered")
+                    .addObject("email", form.getEmail());
+        }
+        return "/form";
+    }
+}
+
+
+/*
         if (bindingResult.hasErrors()) {
             return "/form";
         }
+        Period period = Period.between(form.getBirthDate(), LocalDate.now());
+        int vek = period.getYears();
+        if (vek < 9 || vek > 15) {
+            bindingResult.rejectValue("birthDate","Sorry, but only children in the age of 9-15years can be registered!","not valid age");
+            return "/form";
+        }
+       return new ModelAndView("/registered")
+                    .addObject("email", form.getEmail());
+*/
 
-        return new ModelAndView("/registered")
-                .addObject("email", form.getEmail());
-    }
 
-}
+
--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistrationForm.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistrationForm.java
@@ -1,27 +1,30 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+//import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 public class RegistrationForm {
-    @NotBlank
+    @NotBlank (message = "Name is mandatory.")
     private String firstName;
-    @NotBlank
+    @NotBlank (message = "Name is mandatory.")
     private String lastName;
-    @NotNull
+    @NotNull (message = "Birthdate is mandatory.")
+    @PastOrPresent (message = "Birthdate in future time is not valid.")
+    //@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthDate;
-    @NotBlank
+    @NotEmpty (message = "Please choose the gender.")
     private String gender;
-    @NotBlank
+    @NotNull (message = "Please choose the tour option.")
     private String tour;
     @Email
     private String email;
 
     private String phoneNumber;
 
+    public RegistrationForm() {
+    }
 
     public String getFirstName() {
         return firstName;
